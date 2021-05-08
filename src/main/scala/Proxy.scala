45b1863e305c194c9f7c97e10c39f1f2d58bb956
envy124 <pkgsail.dev@gmail.com>
2021-05-08 19:24:39 +0300

    add Socks4,5Command codecs

--- src/main/scala/Proxy.scala
+++ src/main/scala/Proxy.scala
@@ -1,25 +1,49 @@
 package org.example.socksproxy
 
+import scodec._
+import scodec.bits._
+import scodec.codecs._
+import scodec.codecs.implicits._
+
 import java.net.{Inet4Address, Inet6Address}
 
 sealed trait Proxy
-case class SocksV4(command: Socks4Command, port: Int, address: Inet4Address, clientId: String) extends Proxy
-case class SocksV4A(command: Socks4Command, port: Int, clientId: String, domain: String) extends Proxy
-case class SocksV5(auth: Socks5Authorization, header: Option[Socks5Header]) extends Proxy
+object Proxy {
+  case class SocksV4(command: Socks4Command, port: Int, address: Inet4Address, clientId: String) extends Proxy
+  object SocksV4 {
+  }
 
-object HttpTunnel extends Proxy
-case class Http(address: String) extends Proxy
+  case class SocksV4A(command: Socks4Command, port: Int, clientId: String, domain: String) extends Proxy
+
+  case class SocksV5(auth: Socks5Authorization, header: Option[Socks5Header]) extends Proxy
+
+  object HttpTunnel extends Proxy
+
+  case class Http(address: String) extends Proxy
+}
 
 
 
 sealed trait Socks4Command
 object Socks4Command {
+  implicit val codec: DiscriminatorCodec[Socks4Command, Int] = mappedEnum(
+    uint8,
+    Socks4Command.Connect -> 1,
+    Socks4Command.PortBinding -> 2
+  )
+
   object Connect extends Socks4Command
-  object UdpAssociation extends Socks4Command
+  object PortBinding extends Socks4Command
 }
 
 sealed trait Socks5Command
 object Socks5Command {
+  implicit val codec: DiscriminatorCodec[Socks5Command, Int] = mappedEnum(
+    uint8,
+    Socks5Command.Connect -> 1,
+    Socks5Command.PortBinding -> 2,
+    Socks5Command.UdpAssociation -> 3
+  )
   object Connect extends Socks5Command
   object PortBinding extends Socks5Command
   object UdpAssociation extends Socks5Command
